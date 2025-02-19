12d9f1c1fb526a0bbce332574d3c2fc76f731663
alex <rusanov-ao@inbox.ru>
2025-02-19 22:12:14 +0300

    add pattern singleton

---
+++ src/main/scala/org/example/singleton/Singleton.scala
@@ -0,0 +1,13 @@
+package org.example.singleton
+
+/**
+ * В scala object уже реализует паттерн Singleton
+ * Он гарантирует создание одного объекта в программе
+ */
+object Singleton {
+
+  def setUp(): Unit = {
+    println("setUp")
+  }
+
+}
