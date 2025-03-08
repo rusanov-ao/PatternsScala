cdd909e64a36ba72c8d3c6eaa1ddc7c5f69e5a76
alex <rusanov-ao@inbox.ru>
2025-03-08 21:01:25 +0300

    add pattern builder

---
+++ src/main/scala/org/example/builder/Main.scala
@@ -0,0 +1,18 @@
+package org.example.builder
+
+object Main {
+  def main(args: Array[String]): Unit = {
+
+
+  val sportCar = Car.builder().
+    setDoors(2).
+    setTires(19).
+    setColor("red").
+    setHasNavigator(false).
+    setInsurancePolice("Asd")
+
+    println(sportCar)
+
+  }
+
+}
