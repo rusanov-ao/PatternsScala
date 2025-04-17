e21d14f1ffc4feea2fd3617bb21b9e4af4c94cb7
alex <rusanov-ao@inbox.ru>
2025-04-17 23:52:07 +0300

    add pattern duck typing

--- src/main/scala/org/example/functional_patterns/duck_typing/Main.scala
+++ src/main/scala/org/example/functional_patterns/duck_typing/Main.scala
@@ -14,9 +14,19 @@ object Main {
 
   }
 
-    // в качестве параметра мы прописываем структурный тип duck и в {} сам метод с сигнатурой
-  private def makeSound(duck: {def quack: String}): Unit = {
+  // в качестве параметра мы прописываем структурный тип duck и в {} сам метод с сигнатурой
+  //private def makeSound(duck: {def quack: String}): Unit = {
+  // println(duck.quack)
+  //}
+
+  // в качестве параметра мы прописываем структурный тип duck и в {} сам метод с сигнатурой
+  private def makeSound(duck: DuckType): Unit = {
     println(duck.quack)
+  }
+
+  // воспользуемся утиной типизацией и введем новый тип
+  private type DuckType = {
+    def quack: String
   }
 
 }
